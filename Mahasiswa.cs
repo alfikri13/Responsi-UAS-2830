c9f3cec65e0315cbab7903cd103f33d574e8b198
al fikri utomo <61920101+alfikri13@users.noreply.github.com>
2020-07-08 12:24:56 +0700

    Create Mahasiswa.cs

---
+++ Mahasiswa.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+  public class Mahasiswa
+  {
+    // PERINTAH: lengkapi property class Mahasiswa, sesuai petunjuk soal
+    public string NIM {get; set;}
+    public string Nama {get; set;}
+    public double JenisKelamin {get; set;}
+    public double IPK {get; set;}
+  }
+}
